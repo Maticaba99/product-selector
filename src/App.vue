01402e3f6412d1581dc2b90c0d19b07709bf8cc5
Matias Caballero <maticaba99@hotmail.com>
2021-08-12 19:18:04 -0300

    asd

--- src/App.vue
+++ src/App.vue
@@ -80,14 +80,15 @@ export default {
       this.value = this.element.value ? JSON.parse(this.element.value) : null;
       const language = this.context.variant.codename;
 
-      const valueUpdated = await this.value.map(async item => {
-        return await fetch(this.element.config.API, {
-          method: "post",
-          headers: {
-            Authorization: `Basic ${this.element.config.API_AUTH}`,
-            "Content-Type": "application/json"
-          },
-          body: `{
+      const valueUpdated = await Promise.all(
+        this.value.map(async item => {
+          return await fetch(this.element.config.API, {
+            method: "post",
+            headers: {
+              Authorization: `Basic ${this.element.config.API_AUTH}`,
+              "Content-Type": "application/json"
+            },
+            body: `{
     "query": {
         "bool": {
             "must": [
@@ -105,30 +106,30 @@ export default {
         }
     }
 }`
-        })
-          .then(response => response.json())
-          .then(async json => {
-            const options = await json.hits.hits.map(product => {
-              return {
-                id: product._source.productfields.unique_id,
-                name: product._source.productfields.product_name,
-                dimensions:
-                  product._source.productcard &&
-                  product._source.productcard.dimensionsin,
-                image:
-                  product._source.productcard &&
-                  product._source.productcard.featureimage,
-                quantity: 1,
-                price_cad: product._source.productfields.listprice_cad,
-                price_usd: product._source.productfields.listprice_usd
-              };
+          })
+            .then(response => response.json())
+            .then(async json => {
+              const options = await json.hits.hits.map(product => {
+                return {
+                  id: product._source.productfields.unique_id,
+                  name: product._source.productfields.product_name,
+                  dimensions:
+                    product._source.productcard &&
+                    product._source.productcard.dimensionsin,
+                  image:
+                    product._source.productcard &&
+                    product._source.productcard.featureimage,
+                  quantity: 1,
+                  price_cad: product._source.productfields.listprice_cad,
+                  price_usd: product._source.productfields.listprice_usd
+                };
+              });
+              const selectedPrevious = options.length > 0 && options[0];
+
+              return selectedPrevious;
             });
-            const selectedPrevious = options.length > 0 && options[0];
-
-            return selectedPrevious;
-          });
-      });
-
+        })
+      );
       // eslint-disable-next-line no-console
       console.log(await valueUpdated);
       this.loaded = true;
